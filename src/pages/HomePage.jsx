e940bdc265c7df9daf0c646467b291435be45b1c
stephipie <stephi.pietschmann@gmail.com>
2025-01-28 09:37:05 +0100

    add global layout folder  for react app

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,13 +1,14 @@
-import Footer from "../components/Footer";
-import Header from "../components/Header";
+// import Footer from "../components/Footer";
+// import Header from "../components/Header";
 import ProductList from "../components/ProductList";
+import Layout from "../layout/Layout";
 
 function HomePage() {
   return (
     <div>
-      <Header />
+      <Layout>
       <ProductList />
-      <Footer />
+      </Layout>
     </div>
   );
 }
